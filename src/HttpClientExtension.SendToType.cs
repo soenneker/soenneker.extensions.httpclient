cb2a42797b194ca1ab9058c204320722f4057911
Jake Soenneker <jake@soenneker.com>
2024-10-21 15:16:12 -0500

    Parameter mod

--- src/HttpClientExtension.SendToType.cs
+++ src/HttpClientExtension.SendToType.cs
@@ -13,27 +13,29 @@ namespace Soenneker.Extensions.HttpClient;
 
 public static partial class HttpClientExtension
 {
-    public static ValueTask<TResponse?> SendToType<TResponse>(this System.Net.Http.HttpClient client, string uri,
-        ILogger? logger = null, CancellationToken cancellationToken = default)
+    public static ValueTask<TResponse?> SendToType<TResponse>(this System.Net.Http.HttpClient client, string uri, ILogger? logger = null, CancellationToken cancellationToken = default)
     {
         using var request = new System.Net.Http.HttpRequestMessage(HttpMethod.Get, uri);
 
         return SendToType<TResponse>(client, request, logger, cancellationToken);
     }
 
-    public static ValueTask<TResponse?> SendToType<TResponse>(this System.Net.Http.HttpClient client, HttpMethod httpMethod, string uri, object request,
+    public static ValueTask<TResponse?> SendToType<TResponse>(this System.Net.Http.HttpClient client, HttpMethod httpMethod, string uri, object? request = null,
         ILogger? logger = null, CancellationToken cancellationToken = default)
     {
         using var requestMessage = new System.Net.Http.HttpRequestMessage(httpMethod, uri);
 
-        try
+        if (request != null)
         {
-            requestMessage.Content = request.ToHttpContent();
-        }
-        catch (Exception ex)
-        {
-            logger?.LogError(ex, "Could not build HttpRequestMessage for request type ({type})", request.GetType().Name);
-            return default;
+            try
+            {
+                requestMessage.Content = request.ToHttpContent();
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "Could not build HttpRequestMessage for request type ({type})", request.GetType().Name);
+                return default;
+            }
         }
 
         return SendToType<TResponse>(client, requestMessage, logger, cancellationToken);
