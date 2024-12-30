d0444f662ac78ec766bfc3b2efea4f27263abf41
Jake Soenneker <jake@soenneker.com>
2024-12-30 10:24:03 -0600

    Addt methods

--- src/HttpClientExtension.SendToString.cs
+++ src/HttpClientExtension.SendToString.cs
@@ -4,11 +4,34 @@ using Soenneker.Extensions.Task;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Soenneker.Extensions.ValueTask;
 
 namespace Soenneker.Extensions.HttpClient;
 
 public static partial class HttpClientExtension
 {
+    /// <summary>
+    /// Sends an HTTP GET request to the specified URI and retrieves the response content as a string.
+    /// </summary>
+    /// <param name="client">The <see cref="System.Net.Http.HttpClient"/> instance used to send the request.</param>
+    /// <param name="uri">The URI to which the GET request will be sent.</param>
+    /// <param name="logger">An optional <see cref="ILogger"/> instance for logging errors or additional information.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>
+    /// A <see cref="ValueTask{TResult}"/> containing the response content as a string.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="client"/> or <paramref name="uri"/> is null or empty.</exception>
+    /// <exception cref="HttpRequestException">Thrown if the HTTP request fails.</exception>
+    /// <exception cref="TaskCanceledException">Thrown if the request is canceled via the <paramref name="cancellationToken"/>.</exception>
+
+    public static async ValueTask<string> SendToString(this System.Net.Http.HttpClient client, string uri, ILogger? logger = null,
+        CancellationToken cancellationToken = default)
+    {
+        using var request = new System.Net.Http.HttpRequestMessage(HttpMethod.Get, uri);
+
+        return await client.SendToString(request, logger, cancellationToken).NoSync();
+    }
+
     /// <summary>
     /// Sends an HTTP request and retrieves the response content as a string.
     /// </summary>
--- src/HttpClientExtension.TrySendToString.cs
+++ src/HttpClientExtension.TrySendToString.cs
@@ -3,11 +3,36 @@ using Soenneker.Extensions.Task;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
 
 namespace Soenneker.Extensions.HttpClient;
 
 public static partial class HttpClientExtension
 {
+    /// <summary>
+    /// Tries to send an HTTP GET request to the specified URI and retrieves the response content as a string, capturing any errors in the provided logger.
+    /// </summary>
+    /// <param name="client">The <see cref="System.Net.Http.HttpClient"/> instance used to send the request.</param>
+    /// <param name="uri">The URI to which the GET request will be sent.</param>
+    /// <param name="logger">An optional <see cref="ILogger"/> instance for logging exceptions.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>
+    /// A <see cref="ValueTask{TResult}"/> containing a tuple:
+    /// - <c>successful</c>: A boolean indicating whether the request succeeded.
+    /// - <c>response</c>: The response content as a string, or <c>null</c> if the request failed.
+    /// </returns>
+    /// <remarks>
+    /// If an exception occurs during the request, it is logged (if <paramref name="logger"/> is provided),
+    /// and the method returns <c>(false, null)</c>.
+    /// </remarks>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="client"/> or <paramref name="uri"/> is null or empty.</exception>
+    public static async ValueTask<(bool successful, string? response)> TrySendToString(this System.Net.Http.HttpClient client, string uri, ILogger? logger = null,
+        CancellationToken cancellationToken = default)
+    {
+        using var request = new System.Net.Http.HttpRequestMessage(HttpMethod.Get, uri);
+        return await client.TrySendToString(request, logger, cancellationToken);
+    }
+
     /// <summary>
     /// Tries to send an HTTP request and retrieves the response content as a string, capturing any errors in the provided logger.
     /// </summary>
